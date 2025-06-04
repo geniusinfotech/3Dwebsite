e772c9f73391fa8ee3a930f01842055bf801bd8d
satyam navdiya <satyamnavdiya@gmail.com>
2025-06-04 09:24:59 +0530

    change to destop

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Canvas from "./Canvas";
 import data from "./data.js";
+import LocomotiveScroll from 'locomotive-scroll';
 
 const App = () => {
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
+
   return (
     <>
-      <div className="w-full min-h-screen bg-black text-white">
-        {data.map((item, index) => (
-          <div key={index}>
-            {item.map((canvasdets, index) => (
-              <Canvas key={index} details={canvasdets} />
-            ))}
-          </div>
+      <div className="w-full relative min-h-screen">
+        {data[0].map((canvasdets, index) => (
+          <Canvas key={index} details={canvasdets} />
         ))}
       </div>
+
+      <div className="w-full relative min-h-screen">
+        {data[1].map((canvasdets, index) => (
+          <Canvas key={index} details={canvasdets} />
+        ))}
+      </div>
+
+      <div className="w-full relative min-h-screen">
+        {data[2].map((canvasdets, index) => (
+          <Canvas key={index} details={canvasdets} />
+        ))}
+      </div>
+
+
     </>
   );
 };
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,11 +1,11 @@
-import React, { use, useEffect, useRef } from "react";
-import canvasimages from "./canvasimages";
+import React, { useState, useEffect, useRef } from "react";
+import canvasImages from "./canvasImages";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 const Canvas = ({ details }) => {
   const { startIndex, numImages, duration, size, top, left, zIndex } = details;
-  const [index, setIndex] = React.useState({ value: startIndex });
+  const [index, setIndex] = useState({ value: startIndex });
 
   const canvasRef = useRef(null);
 
@@ -26,19 +26,24 @@ const Canvas = ({ details }) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     const img = new Image();
-    img.src = canvasimages[index.value];
+    img.src = canvasImages[index.value];
     img.onload = () => {
-      canvas.width = img.offsetWidth * scale;
-      canvas.height = img.offsetHeight * scale;
-      canvas.style.width = canvas.offsetWidth +
-      ctx.drawImage(img, 0, 0);
+      canvas.width = canvas.offsetWidth * scale;
+      canvas.height = canvas.offsetHeight * scale;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
+      ctx.scale(scale, scale);
+      ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
     };
   }, [index]);
 
   return (
     <canvas
+    data-scroll 
+    data-scroll-speed={Math.random().toFixed(1)}
       ref={canvasRef}
-      style={{ width: `${size}px`, height: `${size}px` }}
+      className="absolute"
+      style={{ width: `${size*1.8}px`, height: `*${size*1.8}px`, top: `${top}%`, left: `${left}%`, zIndex: `${zIndex}` }}
       id="canvas"
     ></canvas>
   );
